c96ffb926f981d2efc3dae2ae8d2ab6021e5c256
Your Name <you@example.com>
2019-04-07 23:03:59 +0800

    movie

---
+++ js/home/home.js
@@ -0,0 +1,11 @@
+$('#add_favor').on('click', function () {
+    alert("收藏成功");
+});
+
+$('#contact_us').on('click', function () {
+    alert("加入成功");
+});
+
+$('#set_home').on('click', function () {
+    alert("设置成功");
+});
